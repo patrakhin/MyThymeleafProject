ba262644900221c34093166683947594bb6d8eb4
patrakhin <patrakhin@mail.ru>
2023-05-08 21:51:21 +0300

    add log to UserServiceImpl

--- src/main/java/ru/patrakhin/mythymeleafproject/service/UserServiceImpl.java
+++ src/main/java/ru/patrakhin/mythymeleafproject/service/UserServiceImpl.java
@@ -1,12 +1,16 @@
 package ru.patrakhin.mythymeleafproject.service;
 
-import ru.patrakhin.mythymeleafproject.model.User;
-import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.patrakhin.mythymeleafproject.model.User;
 import ru.patrakhin.mythymeleafproject.repository.UserRepository;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Autowired
     private UserRepository userRepository;
@@ -16,7 +20,9 @@ public class UserServiceImpl implements UserService {
         User user = new User();
         user.setUsername(username);
         user.setPassword(password);
-        return userRepository.save(user);
+        User savedUser = userRepository.save(user);
+        LOGGER.info("User created with username: {}", savedUser.getUsername());
+        return savedUser;
     }
 
     @Override
