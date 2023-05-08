0e8eb4b13950ff10bd84eb9ecdd0ac46a9f1b241
patrakhin <patrakhin@mail.ru>
2023-05-08 22:19:17 +0300

    add log for Contact and Home controllers

--- src/main/java/ru/patrakhin/mythymeleafproject/controller/ContactController.java
+++ src/main/java/ru/patrakhin/mythymeleafproject/controller/ContactController.java
@@ -1,14 +1,18 @@
 package ru.patrakhin.mythymeleafproject.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ContactController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ContactController.class);
 
     @GetMapping("/contact")
     public String contact(Model model){
+        LOGGER.info("Contact page visited");
         return "contact";
     }
 }
--- src/main/java/ru/patrakhin/mythymeleafproject/controller/HomeController.java
+++ src/main/java/ru/patrakhin/mythymeleafproject/controller/HomeController.java
@@ -1,5 +1,7 @@
 package ru.patrakhin.mythymeleafproject.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
     @GetMapping("/")
     public String home(Model model) {
+        LOGGER.info("Home page visited");
         // здесь можно добавить данные для передачи на страницу, например:
         model.addAttribute("message", "Welcome to MyThymeleafProject!");
         return "home";
